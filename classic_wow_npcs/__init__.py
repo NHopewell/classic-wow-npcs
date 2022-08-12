1789010134d2aec6039b564bb285961beb2ea324
NHopewell <nicholashopewell@gmail.com>
2022-08-12 20:24:45 +0200

    add npc model

--- classic_wow_npcs/__init__.py
+++ classic_wow_npcs/__init__.py
@@ -4,6 +4,7 @@ import os
 
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the app
@@ -14,7 +15,24 @@ api = Api(app)
 # set config
 app_settings = os.getenv("APP_SETTINGS")
 app.config.from_object(app_settings)
-  # new
+
+# instatiate the db
+db = SQLAlchemy(app)
+
+class NPC(db.Model):
+    __tablename__ = 'npcs'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(128), nullable=False)
+    level = db.Column(db.Integer, nullable=False)
+    faction = db.Column(db.String(128), nullable=False)
+    location = db.Column(db.String(128), nullable=False)
+    health = db.Column(db.Integer, nullable=False)
+    damage = db.Column(db.Integer, nullable=False)
+    armor = db.Column(db.Integer, nullable=False)
+    background = db.Column(db.text())
+
+
 
 class Ping(Resource):
     def get(self):
