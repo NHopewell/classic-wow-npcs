1e0c0978c43588c9f996ed7f187c35dd8ae5eac6
NHopewell <nicholashopewell@gmail.com>
2022-08-12 21:48:59 +0200

    add flask cli command to recreate db from command line

--- manage.py
+++ manage.py
@@ -1,9 +1,16 @@
 from flask.cli import FlaskGroup
 
-from classic_wow_npcs import app
+from classic_wow_npcs import app, db
 
 
 cli = FlaskGroup(app)
+
+
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 
 if __name__ == '__main__':
