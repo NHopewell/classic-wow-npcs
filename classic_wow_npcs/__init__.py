fcbefc66640504fa380ccdd2ab988894c0faf898
NHopewell <nicholashopewell@gmail.com>
2022-08-12 18:42:05 +0200

    add inital app

--- classic_wow_npcs/__init__.py
+++ classic_wow_npcs/__init__.py
@@ -1,1 +1,27 @@
 __version__ = '0.1.0'
+
+import os
+
+from flask import Flask, jsonify
+from flask_restx import Resource, Api
+
+
+# instantiate the app
+app = Flask(__name__)
+
+api = Api(app)
+
+# set config
+app_settings = os.getenv("APP_SETTINGS")
+app.config.from_object(app_settings)
+  # new
+
+class Ping(Resource):
+    def get(self):
+        return {
+            'status': 'success',
+            'message': 'ping pong!'
+        }
+
+
+api.add_resource(Ping, '/ping')
