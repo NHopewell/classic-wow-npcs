e263580ee072dce15489ffe6f8da178afa17c74d
NHopewell <nicholashopewell@gmail.com>
2022-08-13 19:43:08 +0200

    document tests

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -2,14 +2,20 @@ import os
 
 
 def test_development_config(test_app):
+    # given 
     test_app.config.from_object('classic_wow_npcs.config.DevelopmentConfig')
+
+    # then
     assert test_app.config['SECRET_KEY'] == 'my_precious'
     assert not test_app.config['TESTING']
     assert test_app.config['SQLALCHEMY_DATABASE_URI'] == os.environ.get('DATABASE_URL')
 
 
 def test_testing_config(test_app):
+    # given
     test_app.config.from_object('classic_wow_npcs.config.TestingConfig')
+
+    # then
     assert test_app.config['SECRET_KEY'] == 'my_precious'
     assert test_app.config['TESTING']
     assert not test_app.config['PRESERVE_CONTEXT_ON_EXCEPTION']
@@ -17,7 +23,10 @@ def test_testing_config(test_app):
 
 
 def test_production_config(test_app):
+    # given
     test_app.config.from_object('classic_wow_npcs.config.ProductionConfig')
+
+    # then
     assert test_app.config['SECRET_KEY'] == 'my_precious'
     assert not test_app.config['TESTING']
     assert test_app.config['SQLALCHEMY_DATABASE_URI'] == os.environ.get('DATABASE_URL')
