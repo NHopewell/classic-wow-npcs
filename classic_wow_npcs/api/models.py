ce04148a9c013f25e69a65631f42aa422f504588
NHopewell <nicholashopewell@gmail.com>
2022-08-13 21:28:12 +0200

    move NPC model to its own file

---
+++ classic_wow_npcs/api/models.py
@@ -0,0 +1,25 @@
+from classic_wow_npcs import db
+
+
+class NPC(db.Model):
+    __tablename__ = 'npcs'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(128), nullable=False)
+    level = db.Column(db.Integer, nullable=False)
+    faction = db.Column(db.String(128), nullable=False)
+    location = db.Column(db.String(128), nullable=False)
+    health = db.Column(db.Integer, nullable=False)
+    damage = db.Column(db.Integer, nullable=False)
+    armor = db.Column(db.Integer, nullable=False)
+    background = db.Column(db.Text)
+
+    def __init__(self, name, level, faction, health, damage, armor, background):
+        self.name = name
+        self.level = level
+        self.faction = faction
+        self.health = health
+        self.damage = damage
+        self.armor = armor
+        self.background = background
+        
